b98adf6c4c0098f38fb9005be2b088d456573ca3
ayse gungormez <aysegngrmz.91@windowslive.com>
2023-10-30 15:24:43 +0300

    client controller created

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,30 @@
+class ClientsController < ApplicationController
+  def new
+    @client = Client.new
+  end
+
+  def index
+    @clients = Client.all
+  end
+
+  def show
+    @client = Client.find(params[:id])
+  end
+
+  def create
+    @client = Client.new
+
+    if @client.save
+
+      redirect_to clients_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def client_params
+    params.require(:client).permit(:first_name, :last_name)
+  end
+end
