a35cf4f530aa3ac9cbe4ae49df65124fd336ec53
ayse gungormez <aysegngrmz.91@windowslive.com>
2023-11-02 15:40:03 +0300

    editupdate methods added to clients controller and view page created

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,4 +1,6 @@
 class ClientsController < ApplicationController
+  before_action :set_client, only: %i[show edit update destroy]
+
   def new
     @client = Client.new
   end
@@ -8,7 +10,6 @@ class ClientsController < ApplicationController
   end
 
   def show
-    @client = Client.find(params[:id])
     @transactions = @client.transactions
     @expenses = @transactions.where(transaction_type: 'expense')
     @payments = @transactions.where(transaction_type: 'payment')
@@ -25,14 +26,27 @@ class ClientsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @client.update(client_params)
+      redirect_to client_path
+    else
+      render edit
+    end
+  end
+
   def destroy
-    @client = Client.find(params[:id])
     @client.destroy
 
     redirect_to clients_path, status: :see_other
   end
 
   private
+
+  def set_client
+    @client = Client.find(params[:id])
+  end
 
   def client_params
     params.require(:client).permit(:first_name, :last_name)
