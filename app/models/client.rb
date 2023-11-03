bd5e1775ae5833f2cb8aa98a20eb77fb1a49e2e0
ayse gungormez <aysegngrmz.91@windowslive.com>
2023-11-03 15:09:25 +0300

    model validations

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,9 @@
 class Client < ApplicationRecord
   has_many :transactions, dependent: :destroy
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   def balance
     total_payments = transactions.where(transaction_type: 'payment').sum(:amount)
     total_expenses = transactions.where(transaction_type: 'expense').sum(:amount)
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,8 @@
 class Transaction < ApplicationRecord
   belongs_to :client
 
+  validates :amount, presence: true
+  validates :transaction_type, presence: true
+
   enum transaction_type: { expense: 'expense', payment: 'payment' }
 end
