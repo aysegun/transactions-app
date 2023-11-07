132eb2241248f665952b494d67fe9e674a0799c0
ayse gungormez <aysegngrmz.91@windowslive.com>
2023-11-07 11:32:17 +0300

    cases controller created

---
+++ app/controllers/cases_controller.rb
@@ -0,0 +1,46 @@
+class CasesController < ApplicationController
+  before_action :set_client, only: %i[new create]
+  def new
+    @case = @client.cases.new
+  end
+
+  def create
+    @case = @client.cases.new(case_params)
+    if @case.save
+      redirect_to client_path(@client)
+    else
+      render new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @case = Case.find(params[:id])
+  end
+
+  def update
+    @case = Case.find(params[:id])
+    @client = @case.client
+    if @case.update(case_params)
+      redirect_to client_path(@case)
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @case = Case.find(params[:id])
+    @client = @case.client
+    @case.destroy
+    redirect_to client_path(@client), status: :see_other
+  end
+
+  private
+
+  def set_client
+    @client = Client.find(params[:client_id])
+  end
+
+  def case_params
+    params.require(:case).permit(:court, :court_number, :client_id)
+  end
+end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,9 +1,19 @@
 class Transaction < ApplicationRecord
   belongs_to :client
-  belongs_to :case, -> { where transaction_type == 'expense' }
+  belongs_to :case, optional: true
 
   validates :amount, presence: true
   validates :transaction_type, presence: true
 
   enum transaction_type: { expense: 'expense', payment: 'payment' }
+
+  after_create :associate_case_if_expense
+
+  private
+
+  def associate_case_if_expense
+    if transaction_type == 'expense'
+      create_case(court: 'YourCourtName', court_number: 'YourCourtNumber')
+    end
+  end
 end
