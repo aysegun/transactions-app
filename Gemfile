b7bba2d83c1ccfdacc67d85a25bca30630ba1623
ayse gungormez <aysegngrmz.91@windowslive.com>
2023-11-23 12:26:39 +0300

    pg search gem added

--- Gemfile
+++ Gemfile
@@ -48,6 +48,8 @@ gem "sassc-rails"
 gem "bootstrap", "~> 5.2"
 gem "font-awesome-sass", "~> 6.1"
 
+gem "pg_search"
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/models/client.rb
+++ app/models/client.rb
@@ -5,6 +5,13 @@ class Client < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
 
+  include PgSearch::Model
+  pg_search_scope :search_by_first_name_and_last_name,
+    against: %i[ first_name last_name ],
+    using: {
+      tsearch: { prefix: true }
+    }
+
   def balance
     total_payments = transactions.where(transaction_type: 'payment').sum(:amount)
     total_expenses = transactions.where(transaction_type: 'expense').sum(:amount)
