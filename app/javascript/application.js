86c7c6d5ca5ccf72db380613bb86fbfd37e41031
ayse gungormez <aysegngrmz.91@windowslive.com>
2024-01-03 13:41:48 +0300

    error fixed webpack

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,3 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-
-import DisableButtonController from "./controllers/transaction_formcontroller.js"
-Stimulus.register("transaction-form", TransactionFormController)
