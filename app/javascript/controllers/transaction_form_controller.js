e1ab92d432d5ba7d4ec87c1643c9f90c1651e50d
ayse gungormez <aysegngrmz.91@windowslive.com>
2024-01-03 17:47:28 +0300

    transaction_form_controller updated

--- app/javascript/controllers/transaction_form_controller.js
+++ app/javascript/controllers/transaction_form_controller.js
@@ -2,13 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ['caseInfoField', 'courtField', 'courtNumberField', 'transactionType'];
+
   connect() {
-    console.log(`controller connected`)
     this.updateFields();
   }
 
   updateFields() {
     const transactionType = this.transactionTypeTarget.value;
+    console.log('Transaction type changed:', transactionType);
     this.toggleFieldsVisibility(transactionType === 'expense');
   }
 
@@ -19,7 +20,28 @@ export default class extends Controller {
   }
 
   transactionTypeChanged() {
+    const transactionType = this.transactionTypeTarget.value;
+    console.log('Transaction type changed event triggered!');
     this.updateFields();
   }
 
+  caseOptionsChanged() {
+    const selectedType = this.transactionTypeTarget.value;
+
+    // Fetching case options
+    fetch('/shared/case_options?transaction_type=' + selectedType)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text();
+      })
+      .then(data => {
+        // Assuming your caseDropdown is a target with an appropriate identifier
+        this.caseInfoFieldTarget.innerHTML = data;
+      })
+      .catch(error => {
+        console.error('Fetch error:', error);
+      });
+  }
 }
