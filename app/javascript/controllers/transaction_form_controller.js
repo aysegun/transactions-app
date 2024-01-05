f9ff0f681d02badfd5a0938762624b802da8408b
ayse gungormez <aysegngrmz.91@windowslive.com>
2024-01-05 16:37:40 +0300

    stimulus controller updated

--- app/javascript/controllers/transaction_form_controller.js
+++ app/javascript/controllers/transaction_form_controller.js
@@ -29,22 +29,39 @@ export default class extends Controller {
   }
 
   caseOptionsChanged() {
-    const selectedType = this.transactionTypeFieldTarget.value;
+    console.log('Inside caseOptionsChanged');
+    const selectedType = this.transactionTypeFieldTarget.querySelector('select').value;
+    // const selectedType = this.transactionTypeFieldTarget.value;
+    const clientId = this.element.dataset.clientId;
+    console.log('Selected Type:', selectedType);
+    console.log('Client ID:', clientId);
 
-    fetch('/shared/case_options?transaction_type=' + selectedType)
+    if(clientId) {
+
+      fetch(`/clients/${clientId}/case_options?transaction_type=${selectedType}`)
       .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.text();
+          if (!response.ok) {
+              throw new Error(`Network response was not ok (${response.status}: ${response.statusText})`);
+          }
+          return response.json();
       })
-
       .then(data => {
-        this.caseInfoFieldTarget.innerHTML = data;
+          this.caseInfoFieldTarget.innerHTML = data.options_html;
+          this.captureSelectedCaseId();
       })
-
       .catch(error => {
-        console.error('Fetch error:', error);
+          console.error(`Fetch error:`, error.message);
       });
+    }
   }
+
+  captureSelectedCaseId() {
+    const caseDropdown = this.caseInfoFieldTarget.querySelector('select');
+
+    if (caseDropdown) {
+        const selectedCaseId = caseDropdown.value;
+        console.log('Selected Case ID:', selectedCaseId);
+    }
+  }
+
 }
