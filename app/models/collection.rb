0a6d8dedd55bf2801bd4aaaf7f298f9b65e017bf
ayse gungormez <aysegngrmz.91@windowslive.com>
2024-01-18 15:46:07 +0300

    calculation_amount method created, show page updated

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -3,4 +3,19 @@ class Collection < ApplicationRecord
 
   validates :amount, presence: true
   validates :description, presence: true
+
+  def calculation_amount(selected_ratio)
+    case selected_ratio
+    when '9,1%'
+      amount * 9.1 / 100
+    when '4,55%'
+      amount * 4.55 / 100
+    when '2,7%'
+      amount * 2.7 / 100
+    when 'none'
+      amount
+    else
+      amount
+    end
+  end
 end
