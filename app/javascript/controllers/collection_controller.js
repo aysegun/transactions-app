0a203a56cd64f5160c001eda419c29bd8fc25f55
ayse gungormez <aysegngrmz.91@windowslive.com>
2024-03-09 18:52:32 +0300

    cases show page updated

--- app/javascript/controllers/collection_controller.js
+++ app/javascript/controllers/collection_controller.js
@@ -185,7 +185,7 @@ export default class extends Controller {
   sumAmounts() {
     let totalAmount = 0;
 
-    const amountElements = this.element.querySelectorAll('[data-collection-target="amount"]');
+    const amountElements = this.element.querySelectorAll('[data-collection-target="amount_"]');
     amountElements.forEach(amountElement => {
       const amount = parseFloat(amountElement.textContent);
       if (!isNaN(amount)) {
@@ -200,6 +200,11 @@ export default class extends Controller {
         }
       }
     });
+
+    const totalAmountElement = document.getElementById('totalAmount');
+    if (totalAmountElement) {
+        totalAmountElement.textContent = totalAmount.toFixed(2);
+    }
 
     console.log('Total Amount:', totalAmount);
   }
